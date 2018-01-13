ade5f7ed9c0ac4e90a63d11f37852447f9e0b9f4
Jarosław Kantorowicz <jaroslaw.k@protonmail.com>
2018-01-13 12:53:18 +0100

    Gnome entity and database update script

--- php-fpm/start.sh
+++ php-fpm/start.sh
@@ -6,6 +6,12 @@ composer install
 
 chmod 777 /var/www/symfony3/var/ -R
 
+
+#php bin/console doctrine:database:drop --force
+php bin/console doctrine:database:create
+
+php bin/console doctrine:schema:update --force
+
 echo "Installation finished!"
 
 echo "starting php-fpm ..."
---
+++ symfony3/src/AppBundle/Entity/Gnome.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Gnome
+ *
+ * @ORM\Table(name="gnome")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\GnomeRepository")
+ */
+class Gnome
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="strength", type="smallint")
+     */
+    private $strength;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="age", type="smallint")
+     */
+    private $age;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="avatar", type="blob", nullable=true)
+     */
+    private $avatar;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Gnome
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set strength
+     *
+     * @param integer $strength
+     *
+     * @return Gnome
+     */
+    public function setStrength($strength)
+    {
+        $this->strength = $strength;
+
+        return $this;
+    }
+
+    /**
+     * Get strength
+     *
+     * @return int
+     */
+    public function getStrength()
+    {
+        return $this->strength;
+    }
+
+    /**
+     * Set age
+     *
+     * @param integer $age
+     *
+     * @return Gnome
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    /**
+     * Get age
+     *
+     * @return int
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * Set avatar
+     *
+     * @param string $avatar
+     *
+     * @return Gnome
+     */
+    public function setAvatar($avatar)
+    {
+        $this->avatar = $avatar;
+
+        return $this;
+    }
+
+    /**
+     * Get avatar
+     *
+     * @return string
+     */
+    public function getAvatar()
+    {
+        return $this->avatar;
+    }
+}
+
